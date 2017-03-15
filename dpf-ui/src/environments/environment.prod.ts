cfbe577665bc3a7799b7281574f746c6464bac80
Micael Pedrosa <micaelpedrosa@gmail.com>
2017-03-15 11:14:52 +0000

    change prod environment config

--- dpf-ui/src/environments/environment.prod.ts
+++ dpf-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   base: '/screen-dr',
   server: 'ws://demo.dicoogle.com/screen-dr/clt',
-  authProfile: 'http://demo.dicoogle.com/keycloak/auth/realms/dev/account',
+  authProfile: 'http://demo.dicoogle.com/keycloak/auth/realms/screen/account',
   authProvider: 'http://demo.dicoogle.com/keycloak/auth/realms/screen/',
   authClient: 'screen-dr'
 };
